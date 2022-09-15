b86ac944bb30855b484d345d7c644b405a1d10dd
Jancarlos Acevedo <Jancarlos92@me.com>
2022-09-15 04:17:21 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :news_scrappers
+
+root to: "news_scrappers#index"
+
 end
